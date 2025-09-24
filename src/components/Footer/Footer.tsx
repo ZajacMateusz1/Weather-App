8e3d256ce0f48b12b9451b0be0b145e11a78d349
Mateusz Zajac <144244766+ZajacMateusz1@users.noreply.github.com>
2025-09-24 20:23:24 +0200

    Add responsive styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,16 +3,26 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <h2>Links</h2>
-      <a className={styles.link} href="https://open-meteo.com/" target="_blank">
-        Weather data by Open-Meteo.com
-      </a>
-      <a
-        className={styles.link}
-        href="https://github.com/ZajacMateusz1/Weather-App"
-        target="_blank"
-      >
-        GithHub repo
-      </a>
+      <ul className={styles.ul}>
+        <li className={styles.li}>
+          <a
+            className={styles.link}
+            href="https://open-meteo.com/"
+            target="_blank"
+          >
+            Weather data by Open-Meteo.com
+          </a>
+        </li>
+        <li className={styles.li}>
+          <a
+            className={styles.link}
+            href="https://github.com/ZajacMateusz1/Weather-App"
+            target="_blank"
+          >
+            GithHub repo
+          </a>
+        </li>
+      </ul>
     </footer>
   );
 }
